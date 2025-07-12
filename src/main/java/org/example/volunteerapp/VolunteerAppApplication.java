7da1a878e014c50e32616b41068e4699a8112ac2
hyunseong0307 <hslee000307@gmail.com>
2025-07-12 18:43:09 +0900

    project start

---
+++ src/main/java/org/example/volunteerapp/VolunteerAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.volunteerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/volunteerapp/VolunteerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.volunteerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
